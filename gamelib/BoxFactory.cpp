7eb1d0d10ac1b8709bdc8e82d22fb292e9f5b706
Mike Lattanzio <latzio@gmail.com>
2011-03-15 22:09:39 -0400

    Adding BoxFactory.

---
+++ gamelib/BoxFactory.cpp
@@ -0,0 +1,14 @@
+#include "BoxFactory.h"
+
+BoxFactory::BoxFactory()
+{
+}
+
+BoxFactory::~BoxFactory()
+{
+}
+
+std::string BoxFactory::getTitle() 
+{
+    return "Hello Box Fans!";
+}
---
+++ gamelib/BoxFactory.h
@@ -0,0 +1,22 @@
+#ifndef BoxFactory_h
+#define BoxFactory_h
+
+#include <string>
+
+namespace BoxFactory {
+namespace Game {
+
+class BoxFactory {
+    public:
+    BoxFactory();
+    ~BoxFactory();
+
+    std::string getTitle();
+
+    private:
+};
+
+} // Game
+} // BoxFactory
+
+#endif
